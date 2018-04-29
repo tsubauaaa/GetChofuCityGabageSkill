890a764ae27aa52e7e9d4605661ad43143268eb3
tsubauaaa <tsubasa1173@gmail.com>
2018-04-29 16:03:40 +0900

    地区取得処理を改善

--- src/GetChofuCityGabageSkill.py
+++ src/GetChofuCityGabageSkill.py
@@ -11,8 +11,18 @@ def find_district_number(zip_code):
     res = urllib.request.urlopen(req)
     addr_data = json.loads(res.read())
     address3 = addr_data['results'][0]['address3']
-    print(address3)
-    return 4
+
+    if address3 in {"仙川町", "入間町", "若葉町", "緑ケ丘", "国領町"}:
+        district_num = 1
+    elif address3 in {"西つつじケ丘", "菊野台", "飛田給", "上石原", "東つつじケ丘", "富士見町", "野水", "西町"}:
+        district_num = 2
+    elif address3 in {"深大寺東町", "深大寺元町", "布田", "深大寺北町", "深大寺南町", "染地"}:
+        district_num = 3
+    elif address3 in {"調布ケ丘", "柴崎", "多摩川", "下石原", "八雲台", "佐須町", "小島町"}:
+        district_num = 4
+    else:
+        district_num = 1
+    return district_num
 
 
 def fetch_zip_code(api_host, device_id, access_token):
@@ -61,18 +71,18 @@ def lambda_handler(event, context):
     print(intent)
     when = intent['slots']['When']['value']
 
-    garbage = ""
     if when == '今日':
         garbage = '燃えるゴミの日です。'
     elif when == '明日':
         garbage = '燃えないゴミの日は水曜日です。'
 
+    text = "第" + district_num + "地区のごみ出しは" + garbage
     response = {
         'version': '1.0',
         'response': {
             'outputSpeech': {
                 'type': 'PlainText',
-                'text': garbage
+                'text': text
             }
         }
     }
